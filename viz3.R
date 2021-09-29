ea1af41b0e125cd2f06423f254788130a29a9fc6
Michell Hernández <79372966+Michell2hdz@users.noreply.github.com>
2021-09-29 14:47:39 -0500

    prueba permisos repo

--- viz3.R
+++ viz3.R
@@ -11,3 +11,5 @@ viz3<- ggplot(df1, aes(x=reorder(Partido, -RepresentacionProporcional), Represen
 viz3 + theme_classic() + labs(title="Distribución de la Representación Proporcional por partidos", subtitle ="Legislaturas LXIII,LXIV y LXV",
                                 caption = "Fuente: SIL") + theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0.5))+
   xlab("Partidos") + ylab("Cantidad de diputados")
+
+#prueba
